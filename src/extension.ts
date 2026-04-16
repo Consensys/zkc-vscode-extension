0686e74940b61be02e023ed0b686b9192cfdd7ea
DavePearce <dave01001110@gmail.com>
2026-04-16 17:45:30 +1200

    minimal lsp client
    This puts in place a minimal lsp client which assumes the zkc binary is
    installed aleady, and reuses the existing infrastructure for
    communicating with language servers.  Overall, this minimal extension
    does indeed appear to work with the zkc lsp server.

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,32 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  State,
+} from "vscode-languageclient/node";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+let client: LanguageClient;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "zkc" is now active!');
+export function activate(_context: vscode.ExtensionContext) {
+  const serverOptions: ServerOptions = {
+    command: "zkc",
+    args: ["lsp"],
+  };
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('zkc.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ZkC!');
-	});
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "zkc" }],
+  };
 
-	context.subscriptions.push(disposable);
+  client = new LanguageClient("zkc", "ZkC", serverOptions, clientOptions);
+  client.onDidChangeState((event) => {
+    //if (event.newState === State.Running) {
+    client.info("ZkC language server started.");
+    //  }
+  });
+  client.start();
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): Thenable<void> | undefined {
+  return client?.stop();
+}
